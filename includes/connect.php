368901a803a6c664e6518c7038cbaec4687fbe91
david9280 <david9280@mail.ru>
2021-08-18 04:54:17 +0900

    test heroku

--- includes/connect.php
+++ includes/connect.php
@@ -9,15 +9,20 @@
 // | Copyright (c) 2021 mstfoztrk. All rights reserved.
 // +------------------------------------------------------------------------+
 // Database Configuration
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'dizzy_v23');
+// define('DB_SERVER', 'localhost');
+define('DB_SERVER', 'sql211.epizy.com');
+// define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'epiz_29451680');
+// define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'yan19900928');
+// define('DB_DATABASE', 'dizzy_v23');
+define('DB_DATABASE', 'epiz_29451680_dizzy_v23');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die(mysqli_connect_error());
 mysqli_query($db, 'set character_set_results="utf8mb4"');
 mysqli_query($db, "SET SESSION SQL_MODE=REPLACE(@@SQL_MODE, 'ONLY_FULL_GROUP_BY', '') ");
 mysqli_query($db, "SET @@global.sql_mode= 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'; ");
-$base_url = 'http://localhost/';
+// $base_url = 'http://localhost/';
+$base_url = 'https://dizzyv23.herokuapp.com/';
 $serverDocumentRoot = $_SERVER['DOCUMENT_ROOT'];
 $uploadFile = $serverDocumentRoot . '/uploads/files/';
 $xVideos = $serverDocumentRoot . '/uploads/xvideos/';
